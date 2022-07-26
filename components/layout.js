704e13111e36dd27789af6e8173cb5a822101bcd
AECBanana <60141130+AECBanana@users.noreply.github.com>
2022-07-26 15:54:17 +0800

    update

--- components/layout.js
+++ components/layout.js
@@ -1,7 +1,5 @@
 import Image from 'next/image'
-import styles from './layout.module.css'
 import utilStyles from '../styles/utils.module.css'
-
 
 const name = 'AeCw'
 export const siteTitle = 'AeCw Blog'
@@ -44,42 +42,13 @@ export default function Layout({ children, home }) {
                     </div>
                 </div>
             </div>}
-            <div>
-                <header>
-
-
-                    {/* 头像 */}
-                    {/* {<div class='card w-144 bg-base-100 shadow-xl'>
-                        <div class='card-body items-center text-center'>
-                            
-                            <h1 class='card-title'>{name}</h1>
-                            <div class='card-actions '>
-                                <button class="btn btn-sm btn-ghost"><a href='https://github.com/AECBanana'>Github</a></button>
-                                <button class="btn btn-sm btn-ghost"><a href='mailto:aecbanana@outlook.com'>邮箱 / Email</a></button>
-                            </div>
-
-                        </div>
-                    </div>} */}
-
-
-                </header>
-            </div>
             {/* this */}
             <div>
                 <main>{children}</main>
                 {home ? (
-                    <div tabindex="0" class="collapse">
-                        <div class="collapse-title text-center font-medium">
-                            到头了
-                        </div>
-                        <div class="collapse-content text-center">
-                            <p>真的到头了（￣︶￣）↗　</p>
-                        </div>
-                    </div>
-                ) : (
-
                     <></>
-
+                ) : (
+                    <></>
                 )}
             </div>
             <footer class="footer p-10 bg-neutral-focus text-neutral-content">
